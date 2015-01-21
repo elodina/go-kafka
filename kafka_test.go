847ed5d88473fcbfea342b66f8d2bc253a9ae21a
Ivan Lyutov <ivanlyutov@gmail.com>
2015-01-21 17:25:29 +0200

    Switched to go_kafka_client

--- kafka_test.go
+++ kafka_test.go
@@ -5,18 +5,16 @@ import (
 	"fmt"
 	"time"
 	"github.com/stealthly/go-kafka/producer"
-	"github.com/stealthly/go-kafka/consumer"
 	"github.com/Shopify/sarama"
 	"log"
 	"os"
 	"github.com/stealthly/go_kafka_client"
-	"github.com/wvanbergen/kafka/consumergroup"
 )
 
 var brokers = []string{"go-broker:9092"}
 var zookeepers = []string{"go-zookeeper:2181"}
 
-var timeout = 20 * time.Second
+var timeout = 30 * time.Second
 
 func sendAndConsumeRoutine(t *testing.T, quit chan int) {
 	testTopic := fmt.Sprintf("test-simple-%d", time.Now().Unix())
@@ -27,20 +25,17 @@ func sendAndConsumeRoutine(t *testing.T, quit chan int) {
 	go_kafka_client.CreateMultiplePartitionsTopic(zookeepers[0], testTopic, 1)
 	go_kafka_client.EnsureHasLeader(zookeepers[0], testTopic)
 	kafkaProducer := producer.NewKafkaProducer(testTopic, brokers)
-	defer kafkaProducer.Close()
 	fmt.Printf("Sending message %s to topic %s\n", testMessage, testTopic)
 	err := kafkaProducer.SendStringSync(testMessage)
 	if (err != nil) {
 		quit <- 1
 		t.Fatalf("Failed to produce message: %v", err)
 	}
+	kafkaProducer.Close()
 
-	kafkaConsumer := consumer.NewKafkaConsumer(testTopic, testGroupId, brokers, nil)
-	defer kafkaConsumer.Close()
-	fmt.Printf("Trying to consume the message with group %s\n", testGroupId)
 	messageConsumed := false
-	go kafkaConsumer.Read(func(bytes []byte) {
-		message := string(bytes)
+	kafkaConsumer := createConsumer(testGroupId, func(worker *go_kafka_client.Worker, msg *go_kafka_client.Message, taskId go_kafka_client.TaskId) go_kafka_client.WorkerResult {
+		message := string(msg.Value)
 		if (message != testMessage) {
 			t.Errorf("Produced value %s and consumed value %s do not match.", testMessage, message)
 		} else {
@@ -48,12 +43,20 @@ func sendAndConsumeRoutine(t *testing.T, quit chan int) {
 		}
 		messageConsumed = true
 		quit <- 1
+
+		return go_kafka_client.NewSuccessfulResult(taskId)
+	})
+
+	fmt.Printf("Trying to consume the message with group %s\n", testGroupId)
+	go kafkaConsumer.StartStatic(map[string]int{
+		testTopic : 1,
 	})
 
 	time.Sleep(timeout)
 	if !messageConsumed {
 		t.Errorf("Failed to produce and consume a value within %s", timeout)
 	}
+	<-kafkaConsumer.Close()
 	quit <- 1
 }
 
@@ -76,9 +79,9 @@ func sendAndConsumeGroupsRoutine(t *testing.T, quit chan int) {
 	}
 
 	messageCount := 0
-	readFunc := func(consumerId int) func([]byte) {
-		return func(bytes []byte) {
-			message := string(bytes)
+	readFunc := func(consumerId int) go_kafka_client.WorkerStrategy {
+		return func(worker *go_kafka_client.Worker, msg *go_kafka_client.Message, taskId go_kafka_client.TaskId) go_kafka_client.WorkerResult {
+			message := string(msg.Value)
 			if (message != testMessage) {
 				t.Errorf("Produced value %s and consumed value %s do not match.", testMessage, message)
 			} else {
@@ -86,20 +89,31 @@ func sendAndConsumeGroupsRoutine(t *testing.T, quit chan int) {
 			}
 			messageCount++
 			quit <- 1
+
+			return go_kafka_client.NewSuccessfulResult(taskId)
 		}
 	}
 
-	consumer1 := consumer.NewKafkaConsumer(testTopic, testGroupId, brokers, nil)
+	consumer1 := createConsumer(testGroupId, readFunc(1))
 	fmt.Printf("Trying to consume the message with Consumer 1 and group %s\n", testGroupId)
-	go consumer1.Read(readFunc(1))
+	go consumer1.StartStatic(map[string]int{
+		testTopic : 1,
+	})
 
-	consumer2 := consumer.NewKafkaConsumer(testTopic, testGroupId2, brokers, nil)
+	consumer2 := createConsumer(testGroupId2, readFunc(2))
 	fmt.Printf("Trying to consume the message with Consumer 2 and group %s\n", testGroupId2)
-	go consumer2.Read(readFunc(2))
+	go consumer2.StartStatic(map[string]int{
+		testTopic : 1,
+	})
 	time.Sleep(timeout)
 	if (messageCount != 2) {
 		t.Errorf("Failed to produce and consume a value within %s", timeout)
 	}
+
+
+	<-consumer1.Close()
+	<-consumer2.Close()
+
 	quit <- 1
 	quit <- 1
 }
@@ -117,18 +131,20 @@ func consumerGroupsSinglePartitionRoutine(t *testing.T, quit chan int) {
 	kafkaProducer.SendStringSync("2")
 
 	//create a new consumer and try to consume the 2 produced messages
-	config := consumergroup.NewConsumerGroupConfig()
-	config.CommitInterval = 1 * time.Second
-	consumer1 := consumer.NewKafkaConsumerGroup(topic, consumerGroup1, zookeepers, config)
-	fmt.Printf("Trying to consume messages with Consumer 1 and group %s\n", consumerGroup1)
-	messageCount1 := 0
 	waiter1 := make(chan int)
-	go consumer1.Read(func(bytes []byte) {
-		fmt.Printf("Consumed message %s\n", string(bytes))
+	messageCount1 := 0
+	consumer1 := createConsumer(consumerGroup1, func(worker *go_kafka_client.Worker, msg *go_kafka_client.Message, taskId go_kafka_client.TaskId) go_kafka_client.WorkerResult {
+		fmt.Printf("Consumed message %s\n", string(msg.Value))
 		messageCount1++
 		if (messageCount1 == 2) {
 			waiter1 <- 1
 		}
+
+		return go_kafka_client.NewSuccessfulResult(taskId)
+	})
+	fmt.Printf("Trying to consume messages with Consumer 1 and group %s\n", consumerGroup1)
+	go consumer1.StartStatic(map[string]int{
+		topic : 1,
 	})
 
 	//wait until the messages are consumed or time out after 10 seconds
@@ -142,14 +158,15 @@ func consumerGroupsSinglePartitionRoutine(t *testing.T, quit chan int) {
 	consumer1.Close()
 
 	//create one more consumer with the same consumer group and make sure messages are not consumed again
-	config = consumergroup.NewConsumerGroupConfig()
-	config.CommitInterval = 1 * time.Second
-	consumer2 := consumer.NewKafkaConsumerGroup(topic, consumerGroup1, zookeepers, config)
-	fmt.Printf("Trying to consume messages with Consumer 2 and group %s\n", consumerGroup1)
 	waiter2 := make(chan int)
-	go consumer2.Read(func(bytes []byte) {
-		t.Errorf("Consumer 2 consumed a previously consumed message %s\n", string(bytes))
+	consumer2 := createConsumer(consumerGroup1, func(worker *go_kafka_client.Worker, msg *go_kafka_client.Message, taskId go_kafka_client.TaskId) go_kafka_client.WorkerResult {
+		t.Errorf("Consumer 2 consumed a previously consumed message %s\n", string(msg.Value))
 		waiter2 <- 1
+		return go_kafka_client.NewSuccessfulResult(taskId)
+	})
+	fmt.Printf("Trying to consume messages with Consumer 2 and group %s\n", consumerGroup1)
+	go consumer2.StartStatic(map[string]int{
+		topic : 1,
 	})
 
 	fmt.Println("wait to make sure messages are not consumed again")
@@ -166,14 +183,15 @@ func consumerGroupsSinglePartitionRoutine(t *testing.T, quit chan int) {
 	}
 
 	fmt.Println("consume these messages with a consumer group")
-	config = consumergroup.NewConsumerGroupConfig()
-	config.CommitInterval = 1 * time.Second
-	consumer3 := consumer.NewKafkaConsumerGroup(topic, consumerGroup1, zookeepers, config)
 	//total number of consumed messages should be 50 i.e. no duplicate or missing messages within one group
 	messageCount2 := 0
-	go consumer3.Read(func(bytes []byte) {
-		fmt.Printf("Consumer 1 consumed message %s\n", string(bytes))
+	consumer3 := createConsumer(consumerGroup1, func(worker *go_kafka_client.Worker, msg *go_kafka_client.Message, taskId go_kafka_client.TaskId) go_kafka_client.WorkerResult {
+		fmt.Printf("Consumer 1 consumed message %s\n", string(msg.Value))
 		messageCount2++
+		return go_kafka_client.NewSuccessfulResult(taskId)
+	})
+	go consumer3.StartStatic(map[string]int{
+		topic : 1,
 	})
 
 	<-time.After(timeout / 2)
@@ -185,7 +203,10 @@ func consumerGroupsSinglePartitionRoutine(t *testing.T, quit chan int) {
 
 	//shutdown gracefully
 	kafkaProducer.Close()
-	consumer3.Close()
+	<-consumer1.Close()
+	<-consumer2.Close()
+	<-consumer3.Close()
+
 	quit <- 1
 }
 
@@ -204,15 +225,14 @@ func consumerGroupsMultiplePartitionsRoutine(t *testing.T, quit chan int) {
 	}()
 
 	fmt.Println("consume these messages with a consumer group")
-	config := consumergroup.NewConsumerGroupConfig()
-	config.CommitInterval = 1 * time.Second
-	consumer1 := consumer.NewKafkaConsumerGroup(topic, "group1", zookeepers, config)
-
 	messageCount := 0
-
-	go consumer1.Read(func(bytes []byte) {
-		fmt.Printf("Consumer group consumed message %s\n", string(bytes))
+	consumer1 := createConsumer("group1", func(worker *go_kafka_client.Worker, msg *go_kafka_client.Message, taskId go_kafka_client.TaskId) go_kafka_client.WorkerResult {
+		fmt.Printf("Consumer group consumed message %s\n", string(msg.Value))
 		messageCount++
+		return go_kafka_client.NewSuccessfulResult(taskId)
+	})
+	go consumer1.StartStatic(map[string]int{
+		topic : 1,
 	})
 
 	<-time.After(timeout / 2)
@@ -256,4 +276,29 @@ func TestConsumerGroupsMultiplePartitions(t *testing.T) {
 
 	go consumerGroupsMultiplePartitionsRoutine(t, quit)
 	<-quit
+}
+
+func createConsumer(group string, workerStrategy go_kafka_client.WorkerStrategy) *go_kafka_client.Consumer {
+	//Coordinator settings
+	zookeeperConfig := go_kafka_client.NewZookeeperConfig()
+	zookeeperConfig.ZookeeperConnect = []string{ zookeeper }
+
+	//Actual consumer settings
+	consumerConfig := go_kafka_client.DefaultConsumerConfig()
+	consumerConfig.Coordinator = go_kafka_client.NewZookeeperCoordinator(zookeeperConfig)
+	consumerConfig.Groupid = group
+	consumerConfig.NumWorkers = 1
+	consumerConfig.NumConsumerFetchers = 1
+	consumerConfig.FetchBatchSize = 1
+	consumerConfig.FetchBatchTimeout = 1 * time.Second
+	consumerConfig.Strategy = workerStrategy
+	consumerConfig.AutoOffsetReset = go_kafka_client.SmallestOffset
+	consumerConfig.WorkerFailureCallback = func(*go_kafka_client.WorkerManager) go_kafka_client.FailedDecision {
+		return go_kafka_client.CommitOffsetAndContinue
+	}
+	consumerConfig.WorkerFailedAttemptCallback = func(*go_kafka_client.Task, go_kafka_client.WorkerResult) go_kafka_client.FailedDecision {
+		return go_kafka_client.CommitOffsetAndContinue
+	}
+
+	return go_kafka_client.NewConsumer(consumerConfig)
 }
