1b58b2b1af0b4a9e165b80720a0fb9c269bd0166
serejja <serejja@gmail.com>
2015-03-13 19:32:15 +0200

    integrating with kafka avro encoder/decoder. re #15

--- scala_go_kafka.go
+++ scala_go_kafka.go
@@ -1,33 +1,15 @@
 package main
 
 import (
-	"bytes"
 	"code.google.com/p/go-uuid/uuid"
 	"fmt"
 	"github.com/stealthly/go-avro"
 	"github.com/stealthly/go-kafka/producer"
 	"github.com/stealthly/go_kafka_client"
-	"io/ioutil"
-	"math/big"
 	"os"
 	"os/signal"
 	"time"
 )
-
-type PingPong struct {
-	Counter int64
-	Name    string
-	Uuid    string
-}
-
-//custom string representation to match Scala version. just to simplify reading the console output
-func (p *PingPong) String() string {
-	return fmt.Sprintf("{\"counter\": %d, \"name\": \"%s\", \"uuid\": \"%s\"}", p.Counter, p.Name, p.Uuid)
-}
-
-var schemaRegistry = map[int64]string{
-	int64(0): "./scalago.avsc",
-}
 
 var readTopic string
 var writeTopic string
@@ -35,13 +17,18 @@ var group = "ping-pong-go-group"
 
 var broker = "localhost:9092"
 var zookeeper = "localhost:2181"
+var schemaRepo = "http://localhost:8081"
 
 var kafkaProducer *producer.KafkaProducer = nil
 var kafkaConsumer *go_kafka_client.Consumer = nil
 
+var decoder = go_kafka_client.NewKafkaAvroDecoder(schemaRepo)
+var encoder = go_kafka_client.NewKafkaAvroEncoder(schemaRepo)
+
 func main() {
 	parseArgs()
 
+	go_kafka_client.Logger = go_kafka_client.NewDefaultLogger(go_kafka_client.ErrorLevel)
 	kafkaProducer = producer.NewKafkaProducer(writeTopic, []string{broker})
 
 	//Coordinator settings
@@ -50,36 +37,47 @@ func main() {
 
 	//Actual consumer settings
 	consumerConfig := go_kafka_client.DefaultConsumerConfig()
-    consumerConfig.AutoOffsetReset = go_kafka_client.SmallestOffset
+	consumerConfig.AutoOffsetReset = go_kafka_client.SmallestOffset
 	consumerConfig.Coordinator = go_kafka_client.NewZookeeperCoordinator(zookeeperConfig)
 	consumerConfig.Groupid = group
 	consumerConfig.NumWorkers = 1
 	consumerConfig.NumConsumerFetchers = 1
 
-	p := &PingPong{}
-
 	consumerConfig.Strategy = func(worker *go_kafka_client.Worker, message *go_kafka_client.Message, taskId go_kafka_client.TaskId) go_kafka_client.WorkerResult {
 		time.Sleep(2 * time.Second)
-		camus := decode(p, message.Value)
-		fmt.Printf("golang > received %v\n", p)
-		modify(p)
-		if err := kafkaProducer.SendBytesSync(encode(p, camus.schemaId)); err != nil {
+		recInterface, err := decoder.Decode(message.Value)
+		if err != nil {
+			panic(err)
+		}
+		record, ok := recInterface.(*avro.GenericRecord)
+		if !ok {
+			panic("Not a *GenericError, but expected one")
+		}
+
+		fmt.Printf("golang > received %s\n", fmt.Sprintf("{\"counter\": %d, \"name\": \"%s\", \"uuid\": \"%s\"}", record.Get("counter"), record.Get("name"), record.Get("uuid")))
+		modify(record)
+		encoded, err := encoder.Encode(record)
+		if err != nil {
+			panic(err)
+		}
+
+		if err := kafkaProducer.SendBytesSync(encoded); err != nil {
 			panic(err)
 		}
 
 		return go_kafka_client.NewSuccessfulResult(taskId)
 	}
 
-    consumerConfig.WorkerFailureCallback = func(_ *go_kafka_client.WorkerManager) go_kafka_client.FailedDecision {
-        return go_kafka_client.CommitOffsetAndContinue
-    }
-    consumerConfig.WorkerFailedAttemptCallback = func(_ *go_kafka_client.Task, _ go_kafka_client.WorkerResult) go_kafka_client.FailedDecision {
-        return go_kafka_client.CommitOffsetAndContinue
-    }
+	consumerConfig.WorkerFailureCallback = func(_ *go_kafka_client.WorkerManager) go_kafka_client.FailedDecision {
+		return go_kafka_client.CommitOffsetAndContinue
+	}
+	consumerConfig.WorkerFailedAttemptCallback = func(_ *go_kafka_client.Task, _ go_kafka_client.WorkerResult) go_kafka_client.FailedDecision {
+		return go_kafka_client.CommitOffsetAndContinue
+	}
 
 	kafkaConsumer = go_kafka_client.NewConsumer(consumerConfig)
 
-	pingPongLoop(p)
+	pingPongLoop()
 }
 
 func parseArgs() {
@@ -91,7 +89,7 @@ func parseArgs() {
 	writeTopic = os.Args[2]
 }
 
-func pingPongLoop(p *PingPong) {
+func pingPongLoop() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	go func() {
@@ -106,73 +104,7 @@ func pingPongLoop(p *PingPong) {
 	})
 }
 
-func modify(obj *PingPong) {
-	obj.Counter++
-	obj.Uuid = uuid.New()
-}
-
-func encode(obj *PingPong, schemaId []byte) []byte {
-	buffer := &bytes.Buffer{}
-	buffer.Write([]byte{CAMUS_MAGIC})
-	buffer.Write(schemaId)
-
-	enc := avro.NewBinaryEncoder(buffer)
-	writer := avro.NewSpecificDatumWriter()
-	writer.SetSchema(schemaById(schemaId))
-
-	writer.Write(obj, enc)
-
-	return buffer.Bytes()
-}
-
-func decode(obj interface{}, bytes []byte) *CamusData {
-	camus := NewCamusData(bytes)
-	camus.Read(obj)
-	return camus
-}
-
-func schemaById(bytes []byte) avro.Schema {
-	id := new(big.Int)
-	id.SetBytes(bytes)
-	schemaFile := schemaRegistry[id.Int64()]
-	if schemaBytes, err := ioutil.ReadFile(schemaFile); err != nil {
-		panic(err)
-	} else {
-		schema, err := avro.ParseSchema(string(schemaBytes))
-		if err != nil {
-			panic(err)
-		}
-		return schema
-	}
-}
-
-var CAMUS_MAGIC byte = byte(0)
-
-type CamusData struct {
-	schemaId    []byte
-	dec         *avro.BinaryDecoder
-	datumReader avro.DatumReader
-}
-
-func NewCamusData(data []byte) *CamusData {
-	dec := avro.NewBinaryDecoder(data)
-	if magic, err := dec.ReadInt(); err != nil {
-		panic(err)
-	} else {
-		if byte(magic) != CAMUS_MAGIC {
-			panic("Wrong Camus magic byte")
-		}
-
-		schemaIdArray := make([]byte, 4)
-		dec.ReadFixed(schemaIdArray)
-		schema := schemaById(schemaIdArray)
-		datumReader := avro.NewSpecificDatumReader()
-		datumReader.SetSchema(schema)
-
-		return &CamusData{schemaIdArray, dec, datumReader}
-	}
-}
-
-func (cd *CamusData) Read(obj interface{}) {
-	cd.datumReader.Read(obj, cd.dec)
+func modify(obj *avro.GenericRecord) {
+	obj.Set("counter", obj.Get("counter").(int64)+1)
+	obj.Set("uuid", uuid.New())
 }
